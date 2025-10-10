b027f9caed204635aa7850eed1c7968c1b1c01ee
yanislavh <81357990+yanislavh@users.noreply.github.com>
2025-10-10 13:58:05 +0300

    Zad 2 4MI0700441

---
+++ Hello, C++ - pt.2/Zadacha2.cpp
@@ -0,0 +1,14 @@
+// yh101025.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    unsigned int number;
+
+    std::cin >> number;
+
+    std::cout << number % 10;
+}
+
